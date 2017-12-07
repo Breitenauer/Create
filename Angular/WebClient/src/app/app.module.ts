22c3a0f989a9e86e0e6607d7a9d8f21d4b8a70e0
Jakob Hanner <Jakob@Jakobs-MacBook-Pro.local>
2017-12-07 19:11:20 +0100

    Update Angular Projekt Design und Funktionalitaet

--- Angular/WebClient/src/app/app.module.ts
+++ Angular/WebClient/src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatRadioModule, MatProgressBarModule, MatCardModule} from '@angular/material';
+import { MatButtonModule, MatRadioModule, MatProgressBarModule, MatCardModule, MatInputModule, MatExpansionModule, MatSelectModule, MatGridListModule, MatSliderModule} from '@angular/material';
 
 import{ RestService } from './rest.service' ;
 import { AppComponent } from './app.component';
 import { MasterComponent } from './master/master.component';
+import { UploadComponent } from './upload/upload.component';
+import { LoginComponent } from './login/login.component';
+import { TimestampsComponent } from './timestamps/timestamps.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MasterComponent
+    MasterComponent,
+    UploadComponent,
+    LoginComponent,
+    TimestampsComponent
   ],
   imports: [
     BrowserModule,
@@ -23,9 +29,14 @@ import { MasterComponent } from './master/master.component';
     HttpClientModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatInputModule,
     MatRadioModule,
     MatProgressBarModule,
-    MatCardModule
+    MatCardModule,
+    MatExpansionModule,
+    MatSelectModule,
+    MatGridListModule,
+    MatSliderModule
   ],
   providers: [RestService],
   bootstrap: [AppComponent]
---
+++ Angular/WebClient/src/app/timestamps/timestamps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timestamps',
+  templateUrl: './timestamps.component.html',
+  styleUrls: ['./timestamps.component.css']
+})
+export class TimestampsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
